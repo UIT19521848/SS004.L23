c5816be7e35db6141a022003ca3f01a6385cfd1a
UIT19522206 <64575840+UIT19522206@users.noreply.github.com>
2021-06-15 22:46:34 +0700

    update snake and keep snake moving

--- Game/main.py
+++ Game/main.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame.locals import *
-
+import time
 
 class Apple:
     def __init__(self, parent_screen):
@@ -19,6 +19,7 @@ class Snake:
         self.block = pygame.image.load("PATH_IMG").convert()
         self.x = 0
         self.y = 0
+        self.direction = 'left'
 
     def draw(self):
         self.parent_screen.fill((110, 110, 5))
@@ -27,21 +28,34 @@ class Snake:
 
 
     def move_left(self):
+        self.direction = 'left'
         self.x -= 10
         self.draw()
 
     def move_right(self):
+        self.direction = 'right'
         self.x += 10
         self.draw()
 
     def move_down(self):
+        self.direction = 'down'
         self.y += 10
         self.draw()
 
     def move_up(self):
+        self.direction = 'up'
         self.y -= 10
         self.draw()
-
+    def keep_moving(self):
+        if self.direction == 'left':
+            self.x -= 10
+        if self.direction == 'right':
+            self.x += 10
+        if self.direction == 'down':
+            self.y += 10
+        if self.direction == 'up':
+            self.y -= 10
+        self.draw()
 
 class Game:
     def __init__(self):
@@ -72,7 +86,8 @@ class Game:
 
                 if event.type == QUIT:
                     running = False
-            pass
+            self.snake.keep_moving()
+            time.sleep(0.2)
 
 if __name__ == "__main__":
     game = Game()
